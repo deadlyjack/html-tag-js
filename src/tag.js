6cdad7a1a31d26d1301a1d8549a536148c55f4da
Ajit Kumar <dellevenjack@gmail>
2025-07-13 15:45:31 +0530

    v2.4.11

--- src/tag.js
+++ src/tag.js
@@ -127,7 +127,7 @@ function create(tagName, children = [], options = {}) {
         break;
 
       default:
-        if (svgElements.includes(prop) && ['number', 'string', 'bigint'].includes(typeof option) || /-/.test(prop)) {
+        if (svgElements.includes(tagName) && ['number', 'string', 'bigint'].includes(typeof option) || /-/.test(prop)) {
           $el.setAttribute(prop, option);
         } else {
           $el[prop] = option;
